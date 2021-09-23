63caa62dde4b075021bf034135defb5246355727
Streaminglink <91237910+Streaminglink@users.noreply.github.com>
2021-09-23 09:33:32 +0530

    Update fsub.py

--- helper_funcs/fsub.py
+++ helper_funcs/fsub.py
@@ -13,7 +13,7 @@ async def handle_force_sub(bot, cmd):
             if user.status == "kicked":
                 await bot.send_message(
                     chat_id=cmd.from_user.id,
-                    text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/JV_Community).",
+                    text="Sorry Sir, You are Banned to use me. Contact my [Master](https://t.me/Aditya1190).",
                     parse_mode="markdown",
                     disable_web_page_preview=True
                 )
@@ -37,7 +37,7 @@ async def handle_force_sub(bot, cmd):
         except Exception:
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Something went Wrong. Contact my [Master](https://t.me/JV_Community).",
+                text="Something went Wrong. Contact my [Master](https://t.me/Aditya1190).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
