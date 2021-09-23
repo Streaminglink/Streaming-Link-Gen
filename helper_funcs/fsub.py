9f4b93a678eacf4f1bf1dda43b799842887b4e91
Streaminglink <91237910+Streaminglink@users.noreply.github.com>
2021-09-23 12:06:03 +0530

    Update fsub.py

--- helper_funcs/fsub.py
+++ helper_funcs/fsub.py
@@ -23,7 +23,7 @@ async def handle_force_sub(bot, cmd):
         except UserNotParticipant:
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+                text="**Please Join My Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
