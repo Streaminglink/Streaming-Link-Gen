46b9e0318570b4f034ea53bbf8be11bd3284f90a
Streaminglink <91237910+Streaminglink@users.noreply.github.com>
2021-09-23 09:31:04 +0530

    Update fsub.py

--- helper_funcs/fsub.py
+++ helper_funcs/fsub.py
@@ -27,7 +27,7 @@ async def handle_force_sub(bot, cmd):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link)
+                            InlineKeyboardButton("🤖 Join My Channel", url=invite_link)
                         ]
                     ]
                 ),
@@ -37,7 +37,7 @@ async def handle_force_sub(bot, cmd):
         except Exception:
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Something went Wrong. Contact my [Support Group](https://t.me/JV_Community).",
+                text="Something went Wrong. Contact my [Master](https://t.me/JV_Community).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
